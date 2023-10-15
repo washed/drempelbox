59152d297ef087edc5552e2c4024f0fd2e8887bf
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-10-15 17:08:33 +0200

    file playback wip

--- src/main.rs
+++ src/main.rs
@@ -24,20 +24,67 @@ use tokio::time;
 use tokio_stream::wrappers::BroadcastStream;
 use tokio_stream::StreamExt as _;
 
-mod file;
-use file::play_file;
+use rodio::source::{SineWave, Source};
+use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+use std::io::BufReader;
+
+use tracing::{error, info};
+use tracing_subscriber;
 
 pub mod ntag215;
 use crate::ntag215::NTAG215;
 
 pub mod ndef;
 
+#[derive(Clone)]
+struct AppState {
+    sender: broadcast::Sender<String>,
+}
+
 #[tokio::main()]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    start_server().await?;
+    tracing_subscriber::fmt::init();
+
+    let (sender, receiver) = broadcast::channel::<String>(16);
+
+    let app_state = AppState { sender };
+
+    let mut join_set = JoinSet::<()>::new();
+
+    start_sink_handler(&mut join_set, receiver).await;
+
+    start_server(app_state).await?;
     // start_ntag_reader().await?;
 
+
+    while let Some(res) = join_set.join_next().await {
+        println!("Task finished unexpectedly!");
+    }
+
     Ok(())
+}
+
+async fn start_sink_handler(join_set: &mut JoinSet<()>, mut receiver: broadcast::Receiver<String>) {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    join_set.spawn(async move {
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        loop {
+            match receiver.recv().await {
+                Ok(sink_message) => {
+                    info!(sink_message, "received sink message");
+                    let file = BufReader::new(File::open(&sink_message).unwrap());
+                    let source = Decoder::new(file).unwrap();
+                    sink.append(source);
+                    sink.sleep_until_end();
+                    info!(sink_message, "done playing");
+                }
+                Err(e) => error!("Error receiving sink message {e}"),
+            }
+        }
+    });
 }
 
 async fn start_ntag_reader() -> Result<(), Box<dyn std::error::Error>> {
@@ -57,10 +104,11 @@ async fn start_ntag_reader() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
+async fn start_server(app_state: AppState) -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/spotify", get(spotify_url))
-        .route("/file", get(file));
+        .route("/file", get(file))
+        .with_state(app_state);
 
     let bind_address: std::net::SocketAddr = env::var("BIND_ADDRESS")?.parse()?;
     axum::Server::bind(&bind_address)
@@ -89,10 +137,12 @@ struct FileQuery {
 }
 
 #[debug_handler]
-async fn file(file_query: Query<FileQuery>) {
+async fn file(State(state): State<AppState>, file_query: Query<FileQuery>) {
     let file_query: FileQuery = file_query.0;
     let path = file_query.path;
-
-    println!("Filepath: {path}");
-    play_file();
+    info!(path, "Got file play request");
+    match state.sender.send(path) {
+        Ok(res) => info!(res, "submitted file request"),
+        Err(e) => info!("error submitting file request: {e}")
+    };
 }
