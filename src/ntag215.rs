2ff4ada6f3e6b97fa074e11eb05f1c5009b53d56
Marc Freudenberg <freudenbergmarc@gmail.com>
2023-10-28 20:06:24 +0200

    make NTAG215 consts public

--- src/ntag215.rs
+++ src/ntag215.rs
@@ -1,10 +1,9 @@
+use crate::ndef::Message;
 use linux_embedded_hal::Spidev;
 use mfrc522::{
     comm::eh02::spi::{DummyDelay, DummyNSS, SpiInterface},
     Initialized, Mfrc522, Uid,
 };
-
-use crate::ndef::Message;
 
 pub struct NTAG215 {
     pub mfrc522: Mfrc522<SpiInterface<Spidev, DummyNSS, DummyDelay>, Initialized>,
@@ -12,58 +11,56 @@ pub struct NTAG215 {
 }
 
 impl NTAG215 {
-    const PAGE_SIZE_BYTES: usize = 4;
-    const BLOCK_SIZE_BYTES: usize = 16;
-    const BLOCK_PAGE_OFFSET: usize = NTAG215::BLOCK_SIZE_BYTES / NTAG215::PAGE_SIZE_BYTES;
-    const PAGE_COUNT: usize = 135;
-    const TOTAL_BYTES_COUNT: usize = NTAG215::PAGE_COUNT * NTAG215::PAGE_SIZE_BYTES;
-    const FULL_BLOCK_COUNT: usize =
+    pub const PAGE_SIZE_BYTES: usize = 4;
+    pub const BLOCK_SIZE_BYTES: usize = 16;
+    pub const BLOCK_PAGE_OFFSET: usize = NTAG215::BLOCK_SIZE_BYTES / NTAG215::PAGE_SIZE_BYTES;
+    pub const PAGE_COUNT: usize = 135;
+    pub const TOTAL_BYTES_COUNT: usize = NTAG215::PAGE_COUNT * NTAG215::PAGE_SIZE_BYTES;
+    pub const FULL_BLOCK_COUNT: usize =
         NTAG215::PAGE_COUNT * NTAG215::PAGE_SIZE_BYTES / NTAG215::BLOCK_SIZE_BYTES;
-    const PARTIAL_BLOCKS_PAGE_COUNT: usize = (((NTAG215::PAGE_COUNT as f64
-        * NTAG215::PAGE_SIZE_BYTES as f64
-        / NTAG215::BLOCK_SIZE_BYTES as f64)
-        - NTAG215::FULL_BLOCK_COUNT as f64)
-        * NTAG215::BLOCK_SIZE_BYTES as f64
-        / NTAG215::PAGE_SIZE_BYTES as f64) as usize;
+    pub const PARTIAL_BLOCKS_PAGE_COUNT: usize =
+        (((NTAG215::PAGE_COUNT as f64 * NTAG215::PAGE_SIZE_BYTES as f64
+            / NTAG215::BLOCK_SIZE_BYTES as f64)
+            - NTAG215::FULL_BLOCK_COUNT as f64)
+            * NTAG215::BLOCK_SIZE_BYTES as f64
+            / NTAG215::PAGE_SIZE_BYTES as f64) as usize;
 
     // memory region offsets (ends are inclusive)
-    /*
-    const UID_START: u32 = 0;
-    const UID_END: u32 = 8;
+    pub const UID_START: usize = 0;
+    pub const UID_END: usize = 8;
 
-    const INTERNAL_START: u32 = 9;
-    const INTERNAL_END: u32 = 9;
+    pub const INTERNAL_START: usize = 9;
+    pub const INTERNAL_END: usize = 9;
 
-    const LOCK_BYTES_START: u32 = 10;
-    const LOCK_BYTES_END: u32 = 11;
+    pub const LOCK_BYTES_START: usize = 10;
+    pub const LOCK_BYTES_END: usize = 11;
 
-    const CAPABILITY_CONTAINER_START: u32 = 12;
-    const CAPABILITY_CONTAINER_END: u32 = 15;
+    pub const CAPABILITY_CONTAINER_START: usize = 12;
+    pub const CAPABILITY_CONTAINER_END: usize = 15;
 
-    const USER_MEMORY_START: u32 = 16;
-    const USER_MEMORY_END: u32 = 515;
+    pub const USER_MEMORY_START: usize = 16;
+    pub const USER_MEMORY_END: usize = 515;
 
-    const DYNAMIC_LOCK_BYTES_START: u32 = 516;
-    const DYNAMIC_LOCK_BYTES_END: u32 = 518;
+    pub const DYNAMIC_LOCK_BYTES_START: usize = 516;
+    pub const DYNAMIC_LOCK_BYTES_END: usize = 518;
 
-    const RFUI_0_START: u32 = 519;
-    const RFUI_0_END: u32 = 519;
+    pub const RFUI_0_START: usize = 519;
+    pub const RFUI_0_END: usize = 519;
 
-    const CFG_0_START: u32 = 520;
-    const CFG_0_END: u32 = 523;
+    pub const CFG_0_START: usize = 520;
+    pub const CFG_0_END: usize = 523;
 
-    const CFG_1_START: u32 = 524;
-    const CFG_1_END: u32 = 527;
+    pub const CFG_1_START: usize = 524;
+    pub const CFG_1_END: usize = 527;
 
-    const PWD_START: u32 = 528;
-    const PWD_END: u32 = 531;
+    pub const PWD_START: usize = 528;
+    pub const PWD_END: usize = 531;
 
-    const PACK_START: u32 = 532;
-    const PACK_END: u32 = 533;
+    pub const PACK_START: usize = 532;
+    pub const PACK_END: usize = 533;
 
-    const RFUI_1_START: u32 = 534;
-    const RFUI_1_END: u32 = 535;
-    */
+    pub const RFUI_1_START: usize = 534;
+    pub const RFUI_1_END: usize = 535;
 
     pub fn new(
         mut mfrc522: Mfrc522<SpiInterface<Spidev, DummyNSS, DummyDelay>, Initialized>,
