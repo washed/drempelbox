73cee8f70ee2471a0b55c61f3707bdd74b1db09c
Marc Freudenberg <freudenbergmarc@gmail.com>
2024-01-06 22:13:13 +0100

    shutdown wip

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -1,37 +1,47 @@
+use rppal::gpio::{Error, Gpio, InputPin, Level};
 use std::time::Duration;
-
-use rppal::gpio::{Error, Gpio, InputPin};
+use system_shutdown::shutdown;
 use tokio::task::JoinSet;
 use tokio::time::sleep;
-use tracing::{debug, warn};
+use tracing::{debug, error, warn};
 
 #[derive(Clone)]
 pub struct Shutdown {}
 
 impl Shutdown {
     const POWEROFF_GPIO_PIN: u8 = 5;
+    const POWEROFF_HOLD_ITERATIONS: u8 = 4;
 
     pub async fn new(join_set: &mut JoinSet<()>) -> Result<Self, Error> {
-        let pin = Self::get_pin().ok();
-
-        join_set.spawn(async move {
-            // use interrupt instead of polling!
-            debug!("Running shutdown task started");
-            loop {
-                match pin.as_ref() {
-                    Some(pin) => {
-                        let level = pin.read();
-                        debug!(%level, "SHUTDOWN PIN LEVEL");
+        let mut low_counter = 0;
+        match Self::get_pin() {
+            Ok(pin) => {
+                join_set.spawn(async move {
+                    // use interrupt instead of polling!
+                    debug!("shutdown task started");
+                    loop {
+                        match pin.read() {
+                            Level::High => {}
+                            Level::Low => {
+                                low_counter += 1;
+                                debug!(low_counter, "shutdown pin low");
+                                if low_counter >= Self::POWEROFF_HOLD_ITERATIONS {
+                                    warn!("attempting shutdown");
+                                    match shutdown() {
+                                        Ok(_) => println!("Shutting down, bye!"),
+                                        Err(error) => eprintln!("Failed to shut down: {}", error),
+                                    }
+                                }
+                            }
+                        }
                         sleep(Duration::from_secs(1)).await;
                     }
-                    None => {
-                        warn!("shutdown pin not available!");
-                        sleep(Duration::MAX).await;
-                    }
-                }
+                });
             }
-        });
-
+            Err(e) => {
+                error!(%e, "shutdown pin not available!");
+            }
+        }
         Ok(Self {})
     }
 
