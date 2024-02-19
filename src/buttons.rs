49dcaa284d705d10c83bc5068fbf5f49fb7d0868
tmh-alex <alexander.prostejovsky@mobilityhouse.com>
2024-02-19 22:45:51 +0100

    Debounce and react on properly pressed buttons
    Also fixes a pin number, been to lazy to move to separate commit.

--- src/buttons.rs
+++ src/buttons.rs
@@ -15,7 +15,9 @@ enum VolumePins {
 
 impl VolumeButton {
     const VOLUME_UP_PIN: u8 = 17;
-    const VOLUME_DOWN_PIN: u8 = 17;
+    const VOLUME_DOWN_PIN: u8 = 27;
+    const BUTTON_POLLING_INTERVAL_MILLIS: u64 = 100;
+    const BUTTON_POLLING_MAX_COUNT: u32 = 2;
     
     pub async fn new(join_set: &mut JoinSet<()>) -> Result<Self, Error> {
         let volume_up = VolumePins::UP(Self::VOLUME_UP_PIN);
@@ -34,17 +36,26 @@ impl VolumeButton {
         match Self::get_pin(pin_nr) {
             Ok(pin) => {
                 join_set.spawn(async move {
-                    debug!("Volume {:?}  button task on pin {} started!", direction, pin_nr);
+                    info!("Volume {:?}  button task on pin {} started!", direction, pin_nr);
+                    let mut change_count = 0;
                     loop {
                         match pin.read() {
                             Level::High => {
-                                info!("{:?} button {} not pressed", direction, pin.pin());
+                                debug!("{:?} button {} not pressed", direction, pin.pin());
+                                change_count = 0;
                             }
                             Level::Low => {
-                                info!("{:?} button {} pressed", direction, pin.pin());
+                               debug!("{:?} button {} pressed", direction, pin.pin());
+                               if change_count < Self::BUTTON_POLLING_MAX_COUNT {
+                                    change_count += 1;
+                                } else if change_count == Self::BUTTON_POLLING_MAX_COUNT {
+                                    info!("Volume {}!", direction);
+                                    // TODO: Send volume change request here
+                                    change_count = u32::MAX;  // Indicates that button is pressed 
+                                }
                             }
                         }
-                        sleep(Duration::from_secs(1)).await;
+                        sleep(Duration::from_millis(Self::BUTTON_POLLING_INTERVAL_MILLIS)).await;
                     }   
                 });
             }
