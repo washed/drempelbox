c5e588581901aace7c85dd2793645ee6925a2247
Marc Freudenberg <freudenbergmarc@gmail.com>
2024-03-24 22:22:01 +0100

    maybe fix interrupts

--- src/button.rs
+++ src/button.rs
@@ -1,38 +1,54 @@
 #![allow(unused_imports)]
 
+use debounce::EventDebouncer;
 use rppal::gpio::{Error, Gpio, InputPin, Level, Trigger};
 use std::cmp::Ordering::{Equal, Greater, Less};
 use std::time::Duration;
 use tokio::spawn;
 use tokio::sync::broadcast;
-use tokio::time::sleep;
 use tracing::{debug, error, info};
 
 pub struct Button {
-    pub receiver: broadcast::Receiver<()>,
+    pub receiver: broadcast::Receiver<Level>,
+    _pin: InputPin,
 }
 
 impl Button {
     const EVENT_CHANNEL_CAPACITY: usize = 16;
+    const BUTTON_PRESSED_DURATION_MS: u64 = 100;
 
     pub fn new(pin_number: u8) -> Result<Self, Error> {
         let mut pin: InputPin = Gpio::new()?.get(pin_number)?.into_input_pullup();
-        let trigger: Trigger = Trigger::RisingEdge;
-        let (_sender, receiver) = broadcast::channel(Self::EVENT_CHANNEL_CAPACITY);
-        spawn(async move {
-            if let Err(e) = pin.set_async_interrupt(trigger, |level: Level| Self::callback(level)) {
-                error!("Error setting interrupt: {}", e);
-            }
-            loop { sleep(Duration::from_secs(1)).await; } // This is stupid
-        });
-        Ok(Self { receiver })
-    }
+        let trigger: Trigger = Trigger::Both;
+        let (sender, receiver) = broadcast::channel(Self::EVENT_CHANNEL_CAPACITY);
+        let pin_name = pin.pin();
 
-    fn callback(level: Level) {
-        // Desired signature something along the lines of
-        // callback(level: Level, pin: InputPin, sender: Sender)
-        // to be called from the anonymous function in new()
-        info!("Button pressed. Level: {}", level);
-        // Here comes the polling and call to sender.send()
+        let debounce_delay = Duration::from_millis(Button::BUTTON_PRESSED_DURATION_MS);
+        let debouncer = EventDebouncer::new(debounce_delay, move |level: Level| {
+            info!(
+                pin_name=?pin_name,
+                level=?level,
+                "pin {pin_name} debounced level {level}"
+            );
+            if let Err(e) = sender.send(level) {
+                error!("error sending button event message: {}", e)
+            }
+        });
+
+        if let Err(e) = pin.set_async_interrupt(trigger, move |level: Level| {
+            debug!(
+                pin_name=?pin_name,
+                trigger=?trigger, level=?level,
+                "pin {pin_name} triggered on {trigger} level {level}"
+            );
+            debouncer.put(level);
+        }) {
+            error!("Error setting interrupt: {}", e);
+        }
+
+        Ok(Self {
+            receiver,
+            _pin: pin,
+        })
     }
 }
